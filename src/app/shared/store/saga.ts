6c08b45166579b9483134d95f3328568fcfd65cf
JenkJS <jenkjobs@gmail.com>
2023-06-01 13:34:13 +0300

    fix

--- src/app/shared/store/saga.ts
+++ src/app/shared/store/saga.ts
@@ -9,9 +9,9 @@ import { setTxStatus } from '@app/containers/Pools/store/actions';
 import { actions } from '@app/shared/store/index';
 import store from '../../../index';
 
-export async function start() {
-  await Utils.download('./amm.wasm', async (err, bytes) => {
-    await Utils.callApi('ev_subunsub', { ev_txs_changed: true, ev_system_state: true }, (error, result, full) => {
+export  function start() {
+   Utils.download('./amm.wasm',  (err, bytes) => {
+     Utils.callApi('ev_subunsub', { ev_txs_changed: true, ev_system_state: true }, (error, result, full) => {
       if (error) {
         console.log(err);
       }
